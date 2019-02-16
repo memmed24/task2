193505af21b8149dddc074a29144dcfea6bd7b97
Mammad Mammadli <mmammadli@Mammad-Mammadlis-MacBook-Pro.local>
2019-02-17 00:59:41 +0400

    loaders added, initial posts and users changed from empty arrays to null

--- src/components/posts/index.jsx
+++ src/components/posts/index.jsx
@@ -1,44 +1,69 @@
 import React, { Component } from "react";
 import jsonPlaceholder from "../../apis/jsonPlaceholder";
-import { Table, Header } from "semantic-ui-react";
+import {
+  Table,
+  Header,
+  Segment,
+  Dimmer,
+  Loader,
+  Image
+} from "semantic-ui-react";
 class Posts extends Component {
   constructor() {
     super();
     this.state = {
-      posts: []
+      posts: null,
+      isLoading: true
     };
+  }
+
+  componentDidMount() {
     this.fetchPosts();
   }
 
   fetchPosts = async () => {
     const response = await jsonPlaceholder.get("posts");
     this.setState({
-      posts: response.data
+      posts: response.data,
+      isLoading: false
     });
+  };
+
+  renderContent = () => {
+    return this.state.isLoading ? (
+      <Segment>
+        <Dimmer active>
+          <Loader content="Loading" />
+        </Dimmer>
+        <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+      </Segment>
+    ) : (
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>User id</Table.HeaderCell>
+            <Table.HeaderCell>Title</Table.HeaderCell>
+            <Table.HeaderCell>Body</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {this.state.posts.map(({ id, userId, title, body }) => (
+            <Table.Row key={id}>
+              <Table.Cell>{userId}</Table.Cell>
+              <Table.Cell>{title}</Table.Cell>
+              <Table.Cell>{body}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    );
   };
 
   render() {
     return (
       <div className="table-container">
         <Header as="h1">Posts</Header>
-        <Table>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>User id</Table.HeaderCell>
-              <Table.HeaderCell>Title</Table.HeaderCell>
-              <Table.HeaderCell>Body</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {this.state.posts.map(({ id, userId, title, body }) => (
-              <Table.Row key={id}>
-                <Table.Cell>{userId}</Table.Cell>
-                <Table.Cell>{title}</Table.Cell>
-                <Table.Cell>{body}</Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
+        {this.renderContent()}
       </div>
     );
   }
--- src/components/users/index.jsx
+++ src/components/users/index.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-import { Table, Header } from "semantic-ui-react";
+import {
+  Table,
+  Header,
+  Segment,
+  Dimmer,
+  Loader,
+  Image
+} from "semantic-ui-react";
 import jsonPlaceholder from "../../apis/jsonPlaceholder";
 
 class Users extends Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      users: [],
+      isLoading: true
     };
   }
 
@@ -17,41 +25,55 @@ class Users extends Component {
   fetchUsers = async () => {
     const response = await jsonPlaceholder.get("users");
     this.setState({
-      users: response.data
+      users: response.data,
+      isLoading: false
     });
+  };
+
+  renderContent = () => {
+    return this.state.isLoading ? (
+      <Segment>
+        <Dimmer active>
+          <Loader content="Loading" />
+        </Dimmer>
+        <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+      </Segment>
+    ) : (
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Username</Table.HeaderCell>
+            <Table.HeaderCell>Email</Table.HeaderCell>
+            <Table.HeaderCell>Address</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {this.state.users.map(({ id, name, username, email, address }) => (
+            <Table.Row key={id}>
+              <Table.Cell>{name}</Table.Cell>
+              <Table.Cell>{username}</Table.Cell>
+              <Table.Cell>{email}</Table.Cell>
+              <Table.Cell>
+                {`
+              ${address.street} 
+              ${address.suite} 
+              ${address.city} 
+              ${address.zipcode}
+            `}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    );
   };
 
   render() {
     return (
       <div className="table-container">
         <Header as="h1">Users</Header>
-        <Table>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Name</Table.HeaderCell>
-              <Table.HeaderCell>Username</Table.HeaderCell>
-              <Table.HeaderCell>Email</Table.HeaderCell>
-              <Table.HeaderCell>Address</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {this.state.users.map(({ id, name, username, email, address }) => (
-              <Table.Row key={id}>
-                <Table.Cell>{name}</Table.Cell>
-                <Table.Cell>{username}</Table.Cell>
-                <Table.Cell>{email}</Table.Cell>
-                <Table.Cell>
-                  {`
-                    ${address.street} 
-                    ${address.suite} 
-                    ${address.city} 
-                    ${address.zipcode}
-                  `}
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
+        {this.renderContent()}
       </div>
     );
   }
